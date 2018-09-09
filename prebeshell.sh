47eccf9da86cceb73d11a63b6b93faecdcea106b
GodieSillasca <dcasillasgreco@gmail.com>
2018-09-08 23:56:28 -0500

    Se añadió el prompt (sin colores aún) y ahora al leer la contraseña, no se imprime lo que escribe el usuario.

--- prebeshell.sh
+++ prebeshell.sh
@@ -7,6 +7,7 @@
 #Lo anterior, con la finalidad de que los usuarios y las contraseñas estén medio controladas y los archivos no crezcan demasiado
 #Programado por: Casillas Muñoz, D.A. (Prebecario 3)
 #				 Solana Mejía, H.A. (Prebecario 14)
+#Ejecutar el archivo así: "$$ bash prebeshell.sh", para que las opciones de 'read' estén disponibles
 #echo "PS1="\[\033[01;32m\]\u\[\033[01;31m\]en\[\033[0;36m\]->\[\033[0;36m\]\w$\[\033[0;37m\]""
 clear
 usuariosvalidos=~/usuariosregistrados.txt
@@ -23,21 +24,21 @@ if [ $(grep -c $usuario /etc/passwd | tail -n 1) -ne 0 ]; #esta parte fue recicl
 		if [ $(grep -c $usuario $usuariosvalidos) -ne 0 ]; #en este if, se describe qué ocurrirá si el usuario ingresado es un usuario válido, presente en passwd
 			then
 				echo "Ingresa tu contraseña plox"
-				read contrasena
+				read -s contrasena
 				if [ $(grep -c $contrasena $contrasenas) -ne 0 ]; then #tras leer la contraseña, este if decide si hay acceso o no, verificando que la contraseña in
 					clear
 					figlet bienvenido
 				else
 					while [ $(grep -c $contrasena $contrasenas) -eq 0 ]; do #si la contraseña ingresada no está registrada en contrasenas.txt, se pide la misma hasta que haya una coincidencia
 							echo "Contraseña incorrecta\nIngresa la contraseña correcta."
-							read contrasena
+							read -s contrasena
 						done
 					figlet bienvenido
 				fi
 		else
 			echo "Veo que eres un usuario nuevo, ¿qué contraseña quieres usar?"
 			grep -o $usuario /etc/passwd | tail -n 1 >> $usuariosvalidos #esta línea añade al usuario a la lista de usuarios válidos cuando no está registrado
-			read  contrasena
+			read  -s contrasena
 			echo "$contrasena" >> $contrasenas
 		fi
 	else
@@ -45,4 +46,11 @@ if [ $(grep -c $usuario /etc/passwd | tail -n 1) -ne 0 ]; #esta parte fue recicl
 		figlet chau
 		exit 0;
 fi
-read comando
+
+comando="kha"
+while [ "$comando" != "salir" ]; do
+	echo -n "$usuario @"
+	pwd
+	echo -n "-->"
+	read comando
+done
