2ffb24599f88243ee6aff43c9f18726af5df5e46
GodieSillasca <dcasillasgreco@gmail.com>
2018-09-13 18:55:18 -0500

    El script de hora estaba mal implementado, el comando que se usaba mostraba una salida variable, por lo que no siempre mostraba la hora correctamente. Ahora se usa el comando 'date'

--- hora.sh
+++ hora.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #Este script da la hora de la computadora de manera "personalizada"
 echo -n "Son las: "
-uptime | gawk '{print $(NF -9)}'
+date | gawk '{print $(NF -2)}'
 echo "Que tengas buen día, amiguito, estudia mucho y échale ganas <3"
