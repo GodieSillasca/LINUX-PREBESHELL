b8a718d2ac1e4b4f08b57304b6d8c01276a57eb7
amairanysolanamejia <amairany.solanamejia@gmail.com>
2018-09-13 23:33:23 -0500

    Script con los comandos que se pueden utilizar en la prebeshell. Es ayuda para el usuario.

---
+++ ayuda.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Aquí se muetsran los comandos que el usuario puede utilizar para la gran variedad de opciones de esta PREBESHELL creada por CAMD y SOMH.
+echo ""
+echo -e "		\e[101m**********************************\e[0m"
+echo -e "		\e[101m**************AYUDA***************\e[0m"
+echo -e "		\e[101m**********************************\e[0m"
+echo ""
+echo -e "\e[43mEsta shell tiene diversas opciones a las cuales puedes llamar mediante:\e[0m"
+echo ""
+echo -e "\e[32m'hora'\e[0m ----------Imprime en pantalla la hora del sistema."
+echo -e "\e[33m'salir'\e[0m ---------Sale del presente script, de la PREBESHEEL de Any y Diego."
+echo -e "\e[96m'fecha'\e[0m ---------Imprime en pantalla la fecha del sistema."
+echo -e "\e[32m'juego_ahorcado'\e[0m --Permite ingresar al juego de ahorcado."
+echo -e "\e[33m'juego_dados'\e[0m ---- Permite ingresar al juego de los dados"
+echo -e "\e[96m'Infosis'\e[0m ---------Imprime en pantalla la información básica correspondiente al sistema."
+echo -e "\e[32m'prebeplayer'\e[0m -----Permite ingresar al reproducción de música."
+echo -e "\e[33m'buscar'\e[0m ----------Permite realizar la búsqueda de un archivo especificado en una determinada carpeta del sistema."
+echo -e "\e[96m'arbol'\e[0m -----------Muestra de manera gráfica la jerarquía de un directorio."
+echo -e "\e[32m'creditos'\e[0m --------Imprime en pantalla los datos de los autores."
+
