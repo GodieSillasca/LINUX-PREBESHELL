f27cd85db1b1d64e199b50a35ba9a80acb99d283
amairanysolanamejia <amairany.solanamejia@gmail.com>
2018-09-13 23:31:20 -0500

    Script que muestra los datos de los autores.

---
+++ creditos.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Este script proporciona los cŕeditos <3
+echo ""
+echo "*****************************************"
+echo -e "\e[36mUniversidad Nacional Autónoma de México"
+echo -e "      \e[96m Facultad de Ingeniería"
+echo "            PROTECO"
+echo "          Elaboraron:"
+echo "   Casillas Muñoz, Diego Armando #3"
+echo "   Solana Mejía, Haydee Amairany #14"
+echo  "       Curso: GNU/LINUX"
+echo -e "       Septiembre 2018 \e[0m"
+echo "Contacto: dcasillasgreco@gmail.com"
+echo "          amairany.solanamejia@gmail.com"
+echo "****************************************"
+echo ""
