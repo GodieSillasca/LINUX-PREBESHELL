fbb759a2a3a6d6fe482e541151560d4d49d0002f
amairanysolanamejia <amairany.solanamejia@gmail.com>
2018-09-13 21:48:49 -0500

    Este script proporciona la información básica del sistema, la cual debe conocer el usuario.

---
+++ infosys.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+nombrekernel=`uname -s`
+kernel=`uname -r`
+procesador=`uname -p`
+sistope=`uname -o`
+memoria=`free -m`
+usuario=`uname -n`
+
+echo -e "\e[92m Información del sistema\e[0m"
+echo ""
+echo -e "\e[32m Sistema Operativo:${sistope} \e[0m"
+echo -e "\e[92m Kernel:${nombrekernel}\e[0m"
+echo -e "\e[32m Versión del kernel:${kernel}\e[0m"
+echo -e "\e[92m Procesador:${procesador}\e[0m"
+echo -e "\e[32m Memoria RAM y SWAP [MB]:\e[0m"
+echo -e"\e[92m ${memoria}\e[0m"
+echo -e "\e[32m  Usuario:${usuario}\e[0m"
